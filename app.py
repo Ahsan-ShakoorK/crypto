30d806fb797396ad08b798299f38885790a5b602
Ahsan Shakoor <ahsans786191@gmail.com>
2023-05-20 16:17:03 +0500

    ok

--- app.py
+++ app.py
@@ -7,18 +7,16 @@ connection = pymysql.connect(
     host='twenty.mysql.database.azure.com',
     user='ahsan',
     password='name@123',
-    db='ahsandb',
+    db='named',
     charset='utf8mb4',
     cursorclass=pymysql.cursors.DictCursor,
     ssl={'ca': 'DigiCertGlobalRootCA.crt.pem'}
 )
 
-
 # Function to fetch trading data from MySQL for a specific coin
 def fetch_trading_data(coin):
     query = f"""
         SELECT price,
-               SUM(CASE WHEN timestamp >= NOW() - INTERVAL 1 MINUTE THEN volume ELSE 0 END) AS volume_1min,
                SUM(CASE WHEN timestamp >= NOW() - INTERVAL 5 MINUTE THEN volume ELSE 0 END) AS volume_5min,
                SUM(CASE WHEN timestamp >= NOW() - INTERVAL 15 MINUTE THEN volume ELSE 0 END) AS volume_15min,
                SUM(CASE WHEN timestamp >= NOW() - INTERVAL 60 MINUTE THEN volume ELSE 0 END) AS volume_60min
@@ -29,7 +27,13 @@ def fetch_trading_data(coin):
     with connection.cursor() as cursor:
         cursor.execute(query)
         data = cursor.fetchall()
-    return data
+    
+    # Filter out rows where all volume columns are 0
+    df = pd.DataFrame(data)
+    volume_columns = ['volume_5min', 'volume_15min', 'volume_60min']
+    df = df[~(df[volume_columns] == 0).all(axis=1)]
+    
+    return df
 
 def main():
     # Set Streamlit app title and layout
@@ -37,16 +41,13 @@ def main():
     st.write("Market data retrieved from MySQL server")
 
     # Get the list of coins
-    coins = ["sxp", "chess", "blz", "joe", "perl", "ach", "gmt", "xrp", "akro", "zil"]
+    coins = ["sxp", "chess", "blz", "joe", "perl"]
 
     # Create a selectbox for coin selection
     selected_coin = st.selectbox("Select a coin", coins)
 
     # Fetch trading data for the selected coin
-    data = fetch_trading_data(selected_coin)
-
-    # Convert data to pandas DataFrame
-    df = pd.DataFrame(data)
+    df = fetch_trading_data(selected_coin)
 
     # Display the table
     st.table(df)
