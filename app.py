af1498a8a175aa0f8e971a6b69dbfdda889b2113
Ahsan Shakoor <ahsans786191@gmail.com>
2023-05-21 23:50:36 +0500

    postsql

--- app.py
+++ app.py
@@ -1,39 +1,34 @@
 import streamlit as st
-import pymysql.cursors
+import psycopg2
 import pandas as pd
 
-# Establish connection to MySQL server
-connection = pymysql.connect(
-    host='twenty.mysql.database.azure.com',
-    user='ahsan',
-    password='name@123',
-    db='named',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor,
-    ssl={'ca': 'DigiCertGlobalRootCA.crt.pem'}
-)
-
-# Function to fetch trading data from MySQL for a specific coin
+# Establish connection to PostgreSQL server
+def get_db_connection():
+    connection = psycopg2.connect(
+        host='ahsanpost.postgres.database.azure.com',
+        user='ahsan',
+        password='name@123',
+        dbname='postdb',
+        sslmode='require'
+    )
+    return connection
 def fetch_trading_data(coin):
     query = f"""
         SELECT price,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/300)*300) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/300)*300 + 300) THEN volume ELSE 0 END) AS volume_5min,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/300)*300 - 300) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/300)*300) THEN volume ELSE 0 END) AS volume_5min_before,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/900)*900) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/900)*900 + 900) THEN volume ELSE 0 END) AS volume_15min,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/900)*900 - 900) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/900)*900) THEN volume ELSE 0 END) AS volume_15min_before,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/3600)*3600) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/3600)*3600 + 3600) THEN volume ELSE 0 END) AS volume_60min,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/3600)*3600 - 3600) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/3600)*3600) THEN volume ELSE 0 END) AS volume_60min_before
-    FROM {coin}usdt
-    WHERE timestamp >= CURDATE() + INTERVAL 1 SECOND
-    GROUP BY price
+            SUM(CASE WHEN timestamp >= DATE_TRUNC('second', CURRENT_TIMESTAMP) - INTERVAL '5 minutes' AND timestamp < DATE_TRUNC('second', CURRENT_TIMESTAMP) THEN volume ELSE 0 END) AS volume_5min,
+            SUM(CASE WHEN timestamp >= DATE_TRUNC('second', CURRENT_TIMESTAMP) - INTERVAL '10 minutes' AND timestamp < DATE_TRUNC('second', CURRENT_TIMESTAMP) - INTERVAL '5 minutes' THEN volume ELSE 0 END) AS volume_5min_before,
+            SUM(CASE WHEN timestamp >= DATE_TRUNC('second', CURRENT_TIMESTAMP) - INTERVAL '15 minutes' AND timestamp < DATE_TRUNC('second', CURRENT_TIMESTAMP) THEN volume ELSE 0 END) AS volume_15min,
+            SUM(CASE WHEN timestamp >= DATE_TRUNC('second', CURRENT_TIMESTAMP) - INTERVAL '30 minutes' AND timestamp < DATE_TRUNC('second', CURRENT_TIMESTAMP) - INTERVAL '15 minutes' THEN volume ELSE 0 END) AS volume_15min_before,
+            SUM(CASE WHEN timestamp >= DATE_TRUNC('second', CURRENT_TIMESTAMP) - INTERVAL '60 minutes' AND timestamp < DATE_TRUNC('second', CURRENT_TIMESTAMP) THEN volume ELSE 0 END) AS volume_60min,
+            SUM(CASE WHEN timestamp >= DATE_TRUNC('second', CURRENT_TIMESTAMP) - INTERVAL '120 minutes' AND timestamp < DATE_TRUNC('second', CURRENT_TIMESTAMP) - INTERVAL '60 minutes' THEN volume ELSE 0 END) AS volume_60min_before
+        FROM {coin}usdt
+        WHERE timestamp >= CURRENT_DATE
+        GROUP BY price
     """
+    df = pd.read_sql_query(query, get_db_connection())
 
-    with connection.cursor() as cursor:
-        cursor.execute(query)
-        data = cursor.fetchall()
-    
-    # Filter out rows where all volume columns are 0
-    df = pd.DataFrame(data)
+    st.write(df.columns)  # Display column names in Streamlit app
+
     volume_columns = ['volume_5min', 'volume_5min_before', 'volume_15min', 'volume_15min_before', 'volume_60min', 'volume_60min_before']
     df = df[~(df[volume_columns] == 0).all(axis=1)]
     df = df.rename(columns={
@@ -46,14 +41,13 @@ def fetch_trading_data(coin):
     })
     # Round the numbers to 2 decimal places
     df = df.astype(float).round(8)
- 
-    return df
 
+    return df
 
 def main():
     # Set Streamlit app title and layout
     st.title("Cryptocurrency Market Trading Data")
-    st.write("Market data retrieved from MySQL server")
+    st.write("Market data retrieved from PostgreSQL server")
 
     # Get the list of coins
     coins = ["sxp", "chess", "blz", "joe", "perl"]
