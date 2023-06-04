6f81fd2b7c2f0848b257cdbf9e0a0eb76486dbf3
Ahsan Shakoor <ahsans786191@gmail.com>
2023-06-04 20:26:56 +0500

    ok

--- app.py
+++ app.py
@@ -1,72 +1,85 @@
 import streamlit as st
-import pymysql.cursors
+import pyodbc
 import pandas as pd
-import time
+import time 
+from datetime import datetime, timedelta
 
-# Establish connection to MySQL server
-connection = pymysql.connect(
-    host='usa.mysql.database.azure.com',
-    user='ahsan',
-    password='name@123',
-    db='sqldb',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor,
-    ssl={'ca': 'DigiCertGlobalRootCA.crt.pem'}
-)
+def get_db_connection():
+    connection = pyodbc.connect(
+        driver='{SQL Server}',
+        server='A2NWPLSK14SQL-v06.shr.prod.iad2.secureserver.net',
+        database='db_ran',
+        uid='dbahsantrade',
+        pwd='Pak@1947'
+    )
+    return connection
 
-# Function to fetch trading data from MySQL for a specific coin
-def fetch_trading_data(coin):
-    query_5min = f"""
-        SELECT ROUND(price, 4) AS price,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/300)*300) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/300)*300 + 300) THEN volume ELSE 0 END) AS volume_5min,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/300)*300 - 300) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/300)*300) THEN volume ELSE 0 END) AS volume_5min_before,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/900)*900) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/900)*900 + 900) THEN volume ELSE 0 END) AS volume_15min,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/900)*900 - 900) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/900)*900) THEN volume ELSE 0 END) AS volume_15min_before,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/3600)*3600) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/3600)*3600 + 3600) THEN volume ELSE 0 END) AS volume_60min,
-            SUM(CASE WHEN timestamp >= FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/3600)*3600 - 3600) AND timestamp < FROM_UNIXTIME(FLOOR(UNIX_TIMESTAMP(NOW())/3600)*3600) THEN volume ELSE 0 END) AS volume_60min_before
+def fetch_data(coin):
+    query = f"""
+        SELECT price,
+            SUM(CASE WHEN timestamp >= DATEADD(MINUTE, DATEDIFF(MINUTE, 0, GETDATE()) / 300 * 300, 0) AND timestamp < DATEADD(MINUTE, DATEDIFF(MINUTE, 0, GETDATE()) / 300 * 300 + 5, 0) THEN volume ELSE 0 END) AS '5m',
+            SUM(CASE WHEN timestamp >= DATEADD(MINUTE, DATEDIFF(MINUTE, 0, GETDATE()) / 300 * 300 - 5, 0) AND timestamp < DATEADD(MINUTE, DATEDIFF(MINUTE, 0, GETDATE()) / 300 * 300, 0) THEN volume ELSE 0 END) AS '5m_before',
+            SUM(CASE WHEN timestamp >= DATEADD(MINUTE, DATEDIFF(MINUTE, 0, GETDATE()) / 900 * 900, 0) AND timestamp < DATEADD(MINUTE, DATEDIFF(MINUTE, 0, GETDATE()) / 900 * 900 + 15, 0) THEN volume ELSE 0 END) AS '15m',
+            SUM(CASE WHEN timestamp >= DATEADD(MINUTE, DATEDIFF(MINUTE, 0, GETDATE()) / 900 * 900 - 15, 0) AND timestamp < DATEADD(MINUTE, DATEDIFF(MINUTE, 0, GETDATE()) / 900 * 900, 0) THEN volume ELSE 0 END) AS '15m_before',
+            SUM(CASE WHEN timestamp >= DATEADD(HOUR, DATEDIFF(HOUR, 0, GETDATE()), 0) AND timestamp < DATEADD(HOUR, DATEDIFF(HOUR, 0, GETDATE()) + 1, 0) THEN volume ELSE 0 END) AS '60m',
+            SUM(CASE WHEN timestamp >= DATEADD(HOUR, DATEDIFF(HOUR, 0, GETDATE()) - 1, 0) AND timestamp < DATEADD(HOUR, DATEDIFF(HOUR, 0, GETDATE()), 0) THEN volume ELSE 0 END) AS '60m_before'
         FROM {coin}usdt
-        WHERE timestamp >= CURDATE() + INTERVAL 1 SECOND
+        WHERE timestamp >= CAST(GETDATE() AS DATE)
         GROUP BY price
     """
 
-    with connection.cursor() as cursor:
-        cursor.execute(query_5min)
-        data_5min = cursor.fetchall()
+    connection = get_db_connection()
+    df = pd.read_sql_query(query, connection)
+    connection.close()
 
-    # Filter out rows where all volume columns are 0
-    df_5min = pd.DataFrame(data_5min)
-    volume_columns = ['volume_5min', 'volume_5min_before', 'volume_15min', 'volume_15min_before', 'volume_60min', 'volume_60min_before']
-    df_5min = df_5min[~(df_5min[volume_columns] == 0).all(axis=1)]
-    df_5min = df_5min.rename(columns={
-        'volume_5min': '5m',
-        'volume_5min_before': '5m_b',
-        'volume_15min': '15m',
-        'volume_15min_before': '15m_b',
-        'volume_60min': '60m',
-        'volume_60min_before': '60m_b'
-    })
+    return df
+def fetch_daily_data(coin, selected_date):
+    query = f"""
+        SELECT price,
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '00:00:00' AND '03:59:59' THEN volume ELSE 0 END) AS '4h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '04:00:00' AND '07:59:59' THEN volume ELSE 0 END) AS '8h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '08:00:00' AND '11:59:59' THEN volume ELSE 0 END) AS '12h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '12:00:00' AND '15:59:59' THEN volume ELSE 0 END) AS '16h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '16:00:00' AND '19:59:59' THEN volume ELSE 0 END) AS '20h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '20:00:00' AND '23:59:59' THEN volume ELSE 0 END) AS '24h'
+        FROM {coin}usdt
+        WHERE CAST(timestamp AS DATE) = '{selected_date}'
+        GROUP BY price
+    """
 
-    # Display table
-    st.subheader("Trading Data real time")
-    st.write(df_5min)
+    connection = get_db_connection()
+    df = pd.read_sql_query(query, connection)
+    connection.close()
 
-
+    return df
 def main():
     # Set Streamlit app title and layout
-    # st.title("Cryptocurrency Market Trading Data")
-    # st.write("Market data retrieved from MySQL server")
+    st.title("Cryptocurrency Market Data")
+    st.write("Market data retrieved from SQL Server")
 
     # Get the list of coins
-    coins = ["sxp", "chess", "blz", "joe", "perl", "ach", "gmt", "xrp", "akro", "zil", "cfx", "adx", "chz", "bel", "alpaca", "elf", "epx", "pros", "t", "dar", "agix", "mob", "id", "trx", "key", "tru", "amb", "magic", "lina", "lever"]
+    coins = ["sxp", "chess", "blz", "joe", "perl", "ach", "gmt", "xrp", "akro", "zil"]
 
     # Create a selectbox for coin selection
     selected_coin = st.selectbox("Select a coin", coins)
 
     # Fetch trading data for the selected coin
-    fetch_trading_data(selected_coin)
-    time.sleep(5)
-    st.experimental_rerun()
+    df = fetch_data(selected_coin)
 
+    # Display the data
+    st.dataframe(df)
+
+    # Get dates for the last 7 days
+    dates = [(datetime.now() - timedelta(days=i)).date() for i in range(7)]
+
+    # Create a selectbox for date selection
+    selected_date = st.selectbox("Select a date", dates)
+
+    # Fetch daily data for the selected coin
+    daily_df = fetch_daily_data(selected_coin, selected_date)
+
+    # Display the daily data
+    st.dataframe(daily_df)
 
 if __name__ == '__main__':
     main()
