2901532ca2c4a7696c02e9d0e9ee8508507f408d
Ahsan Shakoor <ahsans786191@gmail.com>
2023-06-05 02:22:32 +0500

    ok

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import streamlit as st
-from sqlalchemy import create_engine
+import pymssql
 import pandas as pd
+import time
 
-def get_db_connection():
-    engine = create_engine('mssql+pyodbc://dbahsantrade:Pak@1947@A2NWPLSK14SQL-v06.shr.prod.iad2.secureserver.net/db_ran?driver=ODBC+Driver+17+for+SQL+Server')
-    connection = engine.connect()
-    return connection
+# Establish connection to SQL Server
+connection = pymssql.connect(
+    server='A2NWPLSK14SQL-v06.shr.prod.iad2.secureserver.net',
+    user='dbahsantrade',
+    password='Pak@1947',
+    database='db_ran'
+)
 
-def fetch_data(coin):
+# Function to fetch trading data from SQL Server for a specific coin
+def fetch_trading_data(coin):
     query = f"""
-        SELECT 
-            ROUND(price, 6) AS price,
+        SELECT ROUND(price, 6) AS price,
             SUM(CASE WHEN timestamp >= DATEADD(SECOND, FLOOR(DATEDIFF(SECOND, '19700101', GETDATE())/300)*300, '19700101') AND timestamp < DATEADD(SECOND, FLOOR(DATEDIFF(SECOND, '19700101', GETDATE())/300)*300 + 300, '19700101') THEN volume ELSE 0 END) AS volume_5min,
             SUM(CASE WHEN timestamp >= DATEADD(SECOND, FLOOR(DATEDIFF(SECOND, '19700101', GETDATE())/300)*300 - 300, '19700101') AND timestamp < DATEADD(SECOND, FLOOR(DATEDIFF(SECOND, '19700101', GETDATE())/300)*300, '19700101') THEN volume ELSE 0 END) AS volume_5min_before,
             SUM(CASE WHEN timestamp >= DATEADD(SECOND, FLOOR(DATEDIFF(SECOND, '19700101', GETDATE())/900)*900, '19700101') AND timestamp < DATEADD(SECOND, FLOOR(DATEDIFF(SECOND, '19700101', GETDATE())/900)*900 + 900, '19700101') THEN volume ELSE 0 END) AS volume_15min,
@@ -18,31 +22,44 @@ def fetch_data(coin):
             SUM(CASE WHEN timestamp >= DATEADD(SECOND, FLOOR(DATEDIFF(SECOND, '19700101', GETDATE())/3600)*3600, '19700101') AND timestamp < DATEADD(SECOND, FLOOR(DATEDIFF(SECOND, '19700101', GETDATE())/3600)*3600 + 3600, '19700101') THEN volume ELSE 0 END) AS volume_60min,
             SUM(CASE WHEN timestamp >= DATEADD(SECOND, FLOOR(DATEDIFF(SECOND, '19700101', GETDATE())/3600)*3600 - 3600, '19700101') AND timestamp < DATEADD(SECOND, FLOOR(DATEDIFF(SECOND, '19700101', GETDATE())/3600)*3600, '19700101') THEN volume ELSE 0 END) AS volume_60min_before
         FROM {coin}usdt
-        WHERE timestamp >= GETDATE()
+        WHERE timestamp >= CAST(CAST(GETDATE() AS DATE) AS DATETIME)
         GROUP BY price
     """
-    connection = get_db_connection()
-    df = pd.read_sql_query(query, connection)
-    connection.close()
 
+    with connection.cursor(as_dict=True) as cursor:
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+    # Filter out rows where all volume columns are 0
+    df = pd.DataFrame(data)
+    volume_columns = ['volume_5min', 'volume_5min_before', 'volume_15min', 'volume_15min_before', 'volume_60min', 'volume_60min_before']
+    df = df.loc[~(df[volume_columns] == 0).all(axis=1)]
+    df = df.rename(columns={
+        'volume_5min': '5m',
+        'volume_5min_before': '5m_b',
+        'volume_15min': '15m',
+        'volume_15min_before': '15m_b',
+        'volume_60min': '60m',
+        'volume_60min_before': '60m_b'
+    })
+    df = st.write(df)
     return df
 
 def main():
     # Set Streamlit app title and layout
-    st.title("Cryptocurrency Market Data")
+    st.title("Cryptocurrency Market Trading Data")
     st.write("Market data retrieved from SQL Server")
 
     # Get the list of coins
-    coins = ["sxp", "chess", "blz", "joe", "perl", "ach", "gmt", "xrp", "akro", "zil", "cfx", "adx", "chz", "bel", "alpaca", "elf", "epx", "pros", "t", "dar", "agix", "mob", "id", "trx", "key", "tru", "amb", "magic", "lina", "lever"]
+    coins = ["sxp", "chess", "blz", "joe", "perl", "ach", "gmt", "xrp", "akro", "zil"]
 
     # Create a selectbox for coin selection
     selected_coin = st.selectbox("Select a coin", coins)
 
     # Fetch trading data for the selected coin
-    df = fetch_data(selected_coin)
-
-    # Display the data
-    st.dataframe(df)
+    df = fetch_trading_data(selected_coin)
+    time.sleep(5)
+    st.experimental_rerun()
 
 if __name__ == '__main__':
     main()
