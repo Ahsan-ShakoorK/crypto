fb97774d430e7538a84d14738efe221b18339c9c
Ahsan Shakoor <ahsans786191@gmail.com>
2023-06-04 22:13:23 +0500

    ok

--- app.py
+++ app.py
@@ -33,25 +33,38 @@ def fetch_data(coin):
     connection.close()
 
     return df
+
 def fetch_daily_data(coin, selected_date):
     query = f"""
-        SELECT price, 
-    """
-
-    for hour in range(24):
-        start_time = f"{hour:02d}:00:00"
-        end_time = f"{hour:02d}:59:59"
-        column_name = f"{hour+1}h"
-        query += f"SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '{start_time}' AND '{end_time}' THEN volume ELSE 0 END) AS '{column_name}', "
-
-    query = query.rstrip(", ")  # Remove the trailing comma and space
-    query += f"""
+        SELECT price,
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '00:00:00' AND '00:59:59' THEN volume ELSE 0 END) AS '1h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '01:00:00' AND '01:59:59' THEN volume ELSE 0 END) AS '2h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '02:00:00' AND '02:59:59' THEN volume ELSE 0 END) AS '3h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '03:00:00' AND '03:59:59' THEN volume ELSE 0 END) AS '4h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '04:00:00' AND '04:59:59' THEN volume ELSE 0 END) AS '5h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '05:00:00' AND '05:59:59' THEN volume ELSE 0 END) AS '6h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '06:00:00' AND '06:59:59' THEN volume ELSE 0 END) AS '7h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '07:00:00' AND '07:59:59' THEN volume ELSE 0 END) AS '8h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '08:00:00' AND '08:59:59' THEN volume ELSE 0 END) AS '9h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '09:00:00' AND '09:59:59' THEN volume ELSE 0 END) AS '10h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '10:00:00' AND '10:59:59' THEN volume ELSE 0 END) AS '11h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '11:00:00' AND '11:59:59' THEN volume ELSE 0 END) AS '12h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '12:00:00' AND '12:59:59' THEN volume ELSE 0 END) AS '13h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '13:00:00' AND '13:59:59' THEN volume ELSE 0 END) AS '14h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '14:00:00' AND '14:59:59' THEN volume ELSE 0 END) AS '15h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '15:00:00' AND '15:59:59' THEN volume ELSE 0 END) AS '16h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '16:00:00' AND '16:59:59' THEN volume ELSE 0 END) AS '17h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '17:00:00' AND '17:59:59' THEN volume ELSE 0 END) AS '18h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '18:00:00' AND '18:59:59' THEN volume ELSE 0 END) AS '19h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '19:00:00' AND '19:59:59' THEN volume ELSE 0 END) AS '20h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '20:00:00' AND '20:59:59' THEN volume ELSE 0 END) AS '21h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '21:00:00' AND '21:59:59' THEN volume ELSE 0 END) AS '22h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '22:00:00' AND '22:59:59' THEN volume ELSE 0 END) AS '23h',
+            SUM(CASE WHEN CAST(timestamp AS TIME) BETWEEN '23:00:00' AND '23:59:59' THEN volume ELSE 0 END) AS '24h'
         FROM {coin}usdt
         WHERE CAST(timestamp AS DATE) = '{selected_date}'
         GROUP BY price
     """
-
-    return query
 
 
     connection = get_db_connection()
