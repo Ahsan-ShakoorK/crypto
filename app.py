bc2a9b3fe00d17cefa0152bf5d9e85333d3903e1
Ahsan Shakoor <ahsans786191@gmail.com>
2023-06-06 17:41:53 +0500

    ok

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def fetch_daily_data(coin, selected_date, timeframe):
         column_names = [f"{str(interval).zfill(2)}:00" for interval in interval_list]
 
     query = f"""
-        SELECT CAST(price AS DECIMAL(18, 8)) AS price,
+        SELECT price,
             {', '.join([f"SUM(CASE WHEN DATEPART(MINUTE, timestamp) = {interval} THEN volume ELSE 0 END) AS volume_{interval}{timeframe}" for interval in interval_list])}
         FROM {coin}usdt
         WHERE CONVERT(DATE, timestamp) = '{selected_date}'
