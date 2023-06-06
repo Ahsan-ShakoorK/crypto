5b95bfdc8f07e97884e9bc78f7e7590e04db4075
Ahsan Shakoor <ahsans786191@gmail.com>
2023-06-06 17:45:26 +0500

    ok

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def fetch_daily_data(coin, selected_date, timeframe):
         df = df.loc[~(df[volume_columns] == 0).all(axis=1)]
 
     # Rename the columns for better display
-    df.columns = ['Price'] + column_names
+    df.columns = ['price'] + column_names
 
     # Set the price column as the index
     df['price'] = df['price'].apply(lambda x: f"{x:.6f}")
