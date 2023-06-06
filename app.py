05ed5d2c849286dd434769dea215a1d303c8cbb3
Ahsan Shakoor <ahsans786191@gmail.com>
2023-06-06 23:15:35 +0500

    ok

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def main():
     current_time = pd.to_datetime('now').strftime("%Y-%m-%d %H:%M:%S")
     st.write(f"Current Time: {current_time}")
 
-    time.sleep(10)
+    time.sleep(3)
     st.experimental_rerun()
 
 if __name__ == '__main__':
